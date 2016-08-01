c2e1fb14ec8854c2b0a24ccf2fb776543cd23de3
Dhawan Gajendran Gayash <dgayash@paypal.com>
2016-07-31 19:04:22 -0700

    NQueens Initial solution

--- recursion/NQueens.java
+++ recursion/NQueens.java
@@ -1,46 +1,42 @@
 public class NQueens {
 
     public static void main(String[] args) {
-        String[] s = {"1", "2", "3", "4"};
-        printPermutations(s);
-    }
-    private static void printPermutations(Object[] arr) {
-        printPermutations(arr, 0);
+        solveNQueens(4);
     }
 
-    private static void printPermutations(Object[] arr, int i) {
+    private static void solveNQueens(int n) {
+        int[] arr = new int[n];
+        for (int i = 1; i <= arr.length; i++)
+            arr[i - 1] = i;
+        _solveNQueensForAllSolutions(arr, 0);
+
+    }
+
+    private static void _solveNQueensForAllSolutions(int[] arr, int i) {
         if (i == arr.length) {
-            for (Object obj : arr) System.out.print(" " + obj);
-            System.out.println(); 
+            for (int j : arr) System.out.print(" " + j);
+            System.out.println();
         }
-   
+
         for (int j = i; j < arr.length; j++) {
             swap(arr, i, j);
-            if (isNQueens(arr, i))  // Backtracking
-                printPermutations(arr, i+1);
+            if (isQueenThreatning(arr, i))  // Backtracking
+                _solveNQueensForAllSolutions(arr, i + 1);
             swap(arr, i, j);
         }
     }
 
 
-    private static void swap(Object[] arr, int i, int j) {
-        Object tmp = arr[i];
+    private static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
         arr[i] = arr[j];
         arr[j] = tmp;
     }
 
-    private static boolean isNQueens(Object[] arr, int nextQueen) {
-        int[] str = new int[arr.length];
-
-        for(int i = 0; i < arr.length; i++)
-            str[i] = Integer.parseInt((String)arr[i]);
-
-        for(int j = 0; j < nextQueen; j++) {
-                if (str[nextQueen] == (str[j] - (nextQueen - j)))
-                    return false;
-                if (str[nextQueen] == str[j] + (nextQueen  - j))
-                    return false;
-        }
+    private static boolean isQueenThreatning(int[] game, int nextQueen) {
+        for (int j = 0; j < nextQueen; j++)
+            if (Math.abs(game[nextQueen] - game[j])  == Math.abs(nextQueen - j))
+                return false;
         return true;
     }
 }
