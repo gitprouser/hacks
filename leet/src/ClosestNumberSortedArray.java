74b4ca48c8f2a0728617dff1d2a4895369b13a92
gitprouser <gitresumeuser@gmail.com>
2017-03-31 11:47:13 -0700

    Finishing touches on ClosestNumberSorted

--- leet/src/ClosestNumberSortedArray.java
+++ leet/src/ClosestNumberSortedArray.java
@@ -1,6 +1,7 @@
 /**
  *  Closest Number in Sorted Array
- *  Topic: Given a target number and an integer array A sorted in ascending order, find the index i in A such that A[i] is closest to the given target.
+ *  Topic: Given a target number and an integer array A sorted in ascending order, find the index i in A 
+           such that A[i] is closest to the given target.
  *         Return -1 if there is no element in the array. 
  *  Analysis: The main note of the final selection start or end of the thing on the line
               Sorted array with recovery required.  <4, 5, 6, 1, 2>     here arr[lo] = 4 
@@ -18,4 +19,65 @@
                         |  /
                          /
 
+https://yisuang1186.gitbooks.io/-shuatibiji/closest_number_in_sorted_array.html
  */ 
+
+public ClosestNumberSortedArray {
+  /**
+   * @param A an integer array sorted in ascending order
+   * @param target an integer
+   * @return an integer idx of the closest element of the array to the target
+   */
+  public int closestNumber(ArrayList<Integer> arr, int target) {
+    if (arr.size() == 0) 
+      return -1
+
+    int lo = 0, hi = arr.size();
+    while (lo + 1 < hi) {
+      int mid = lo + (hi - lo) / 2;
+      if (arr[mid] == target) {
+        return mid;
+      } 
+      else if (arr[mid] > target) {
+        hi = mid;    
+      } else {
+        lo = mid;  
+      }
+    }
+    
+    /**
+                   arr[lo]   arr[hi]
+number line 
+            -ve <----+-----+---------> +ve    
+            
+            So target can be either:
+              - left of arr[lo] 
+              - right of arr[hi]
+              - inbetween arr[lo] and arr[hi]
+     */
+    
+    // target is left of arr[lo]
+    if (arr[lo] <= target)
+      return lo;
+    
+    // target is right of arr[hi] 
+    else if (arr[hi] <= target)
+      return hi;
+    
+    // target is between arr[lo] < *target* < arr[hi] 
+    // You will need to check, which element in the array is closer to the target.
+    //    int lowToTarget = target - arr[lo];
+    //    int highToTarget = arr[hi] - target;
+    else (
+      if (arr[hi] - target < target - arr[lo])
+        return hi;
+      else
+        return lo;
+    }
+  }
+  
+  public static void main(String[] args) {
+    ClosestNumberSortedArray cs = new ClosestNumberSortedArray();
+    assert (31, cs.closestNumber(Arrays.asList(10,22,31,41,50), 35)); 
+  }
+}
