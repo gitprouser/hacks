d3a4d5fbe251306a2ccbb57bd0bfd0c66082b779
gitprouser <gitresumeuser@gmail.com>
2017-05-08 00:29:46 -0700

    Update PerfectNumbers.java

--- leet/src/PerfectNumbers.java
+++ leet/src/PerfectNumbers.java
@@ -16,15 +16,63 @@ For egs:
 */
 
 class PerfectNumbers {
-  /**
-    Must take a single integer as an argument and return the count
-    of perfect numbers less than or equal to that integer.
-    
-    Sample I/O: 
-      - 0 return 0.
-      - 6 returns 1.
-      - 28 returns 2.
-  */
-  int findPerfectNumbers(int N){ 
-  }
+    /**
+      Must take a single integer as an argument and return the count
+      of perfect numbers less than or equal to that integer.
+
+      Sample I/O: 
+        - 0 return 0.
+        - 6 returns 1.
+        - 28 returns 2.
+    */
+    int findPerfectNumbers(int N){ 
+    }
+
+   /**
+      Returns an integer denoting the number of perfect 
+      numbers less than or equal to input. 
+      @param input an integer
+      @return an integer
+    */
+    public static int findPefectNumbers(int N) {
+      int cnt = 0;
+      for (int i = 0; i <= input; i++) {
+        if (isPerfectNumb(i))
+          ++cnt;
+      }
+      return cnt;
+    }
+
+   /**
+     Returns a boolean value denoting whether the input is perfect 
+     number or not.
+
+     @param input is an integer
+     @return true if pefect number, else false.
+   */
+    public static boolean isPerfectNumb(int numb) {
+        int[] divisors = new int[500];
+        getDivisors(numb, divisors);
+        boolean isPerfect = false;
+        int sum = 0;
+        for (int i = 0; i < 500; i++)
+            sum += divisors[i];
+        
+        if (sum == input)
+            isPerfect = true;
+
+        return isPerfect;
+    }
+
+    /** 
+       Get all the divisors for a given input Number.
+    */
+    private static void getDivisors(int numb, int[] divisors) {
+        int idx = 0;
+        for (int j = 0; j < numb; j++) {
+            if ((numb / j) * j == numb) {
+                divisors[idx++] = j; 
+            }
+        }
+    }
 }
