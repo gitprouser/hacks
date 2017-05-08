b04d8b41d7fbd966e48a3aa3708a543567a3597d
gitprouser <gitresumeuser@gmail.com>
2017-05-08 00:06:10 -0700

    Create PerfectNumbers.java

---
+++ leet/src/PerfectNumbers.java
@@ -0,0 +1,30 @@
+/**
+A perfect number is defined as a number where the sum of its positive 
+divisors are equal to itself.
+
+https://en.wikipedia.org/wiki/List_of_perfect_numbers
+
+For egs: 
+ - 6 (whose positive divisors are 1, 2, and 3) is 
+    a  perfect number as 1 + 2 + 3 = 6
+ - 10 (whose positive divisors are 1, 2, and 5) is not a 
+    perfect number as 1 + 2 + 5 = 8
+
+0 is not a perfect number. 
+
+
+*/
+
+class PerfectNumbers {
+  /**
+    Must take a single integer as an argument and return the count
+    of perfect numbers less than or equal to that integer.
+    
+    Sample I/O: 
+      - 0 return 0.
+      - 6 returns 1.
+      - 28 returns 2.
+  */
+  int findPerfectNumbers(int N){ 
+  }
+}
